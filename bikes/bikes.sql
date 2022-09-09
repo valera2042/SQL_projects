57164c61014aa151b74346f83fc134d3f8707a3f
Valery Liamtsau <71885827+valera2042@users.noreply.github.com>
2022-09-09 21:41:20 +0200

    Add files via upload

--- bikes/bikes.sql
+++ bikes/bikes.sql
@@ -10,11 +10,11 @@ SELECT * FROM bikes
 --4. Any difference in the longevity of the trips for members and casual users?
 --5. Extract time from the column and check what time during the day was the most the least busy for the bikers
 --6. What are the rush hours for bikers?
---6. Calcuualte the distance length by pifagorean theorem and find days that have the shortest and longest by average distance trips 
---7. What was the average number of trip per day?
---8. What was the percentage of days with trip duration higher than that of the monthly average trip duration?
---9. What were the stations with the highest percentage of trips that ended at the same station?
---10. During what days trip were the longest?
+--7. Calculate the distance length by pifagorean theorem and find days that have the shortest and longest by average distance trips 
+--8. What was the average number of trip per day?
+--9. What was the percentage of days with trip duration higher than that of the monthly average trip duration?
+--10. What were the stations with the highest percentage of trips that ended at the same station?
+--11. During what days trip were the longest?
 
 -- 11. average trip duration
 -- Join, case, iif, ratio
@@ -172,3 +172,71 @@ GROUP BY hour_of_start
 ORDER BY SUM(total_diff_seconds) DESC
 
 -- results: the rush hour is from 3 pm to 4 pm
+
+
+
+--7. Calcuualte the distance length by pifagorean theorem and find days that have the shortest and longest by average distance trips 
+SELECT * FROM bikes
+
+CREATE VIEW part_of_the_check AS
+	(SELECT ride_id,
+	-- get the day part 
+		CASE 
+			WHEN time_casted BETWEEN '00:00:01' AND '05:00:00' THEN 'night'
+			WHEN time_casted BETWEEN '05:00:01' AND '12:00:00' THEN 'morning'
+			WHEN time_casted BETWEEN '12:00:01' AND '17:00:00' THEN 'day'
+			ELSE 'evening'
+		END AS part_of_the_day,
+		total_diff_seconds
+	FROM
+		-- assign new column based on the time
+		(SELECT ride_id, CAST(started_at AS TIME) AS time_casted,
+		DATEDIFF(HOUR, started_at, ended_at) * 3600 + 
+		DATEDIFF(MINUTE, started_at, ended_at) * 60 +
+		DATEDIFF(SECOND, started_at, ended_at) AS total_diff_seconds
+		FROM bikes) lookup_table)
+
+SELECT part_of_the_day, AVG(length_trip) AS max_length_a_day
+FROM
+(SELECT ride_id AS ride, SQRT(POWER(ABS(start_lat - end_lat), 2) + POWER(ABS(start_lng - end_lng),2)) AS length_trip 
+FROM bikes) rides
+
+INNER JOIN
+
+(SELECT * FROM part_of_the_check) view_trips
+ON rides.ride = view_trips.ride_id
+GROUP BY part_of_the_day
+ORDER BY AVG(length_trip) DESC
+
+-- results: distance covered by bikers was relatively the same for morning, day and evening
+
+
+--8. What was the average number of trips per day?
+
+SELECT DATEPART(DAY, started_at) AS started_day, COUNT(ride_id) AS total_rides
+FROM bikes
+GROUP BY DATEPART(DAY, started_at)
+ORDER BY COUNT(ride_id) DESC
+-- last part of the month was the most active
+
+
+--9. What was the percentage of days with trip duration higher than that of the monthly average trip duration?
+
+SELECT * FROM bikes
+
+
+--10. What were the stations with the highest percentage of trips that ended at the same station?
+
+SELECT the_same_station, 
+CAST(same AS float)/CAST(total AS float) AS ratio_yes, 
+CAST(not_the_same AS float)/CAST(total AS float) AS ratio_no 
+
+FROM
+	(SELECT the_same_station, COUNT(ride_id) AS count_trips, SUM(IIF(the_same_station='Yes', 1, 0)) AS same,
+	SUM(IIF(the_same_station='no', 1, 0)) AS not_the_same, 
+	SUM(IIF(the_same_station='Yes', 1, 0)) + SUM(IIF(the_same_station='no', 1, 0)) AS total
+	FROM
+	(SELECT ride_id, IIF(start_station_id = end_station_id, 'yes', 'no') AS the_same_station
+	FROM bikes) gg
+GROUP BY the_same_station) HH
+
